646223faa62a65992d004002fb155cf3f788010d
Neek Sandhu <neek.sandhu@outlook.com>
2016-06-22 00:11:35 -0700

    Updated icons, refactored code

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,32 +1,66 @@
-chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-    console.log("request received", request);
-    if(typeof request === "object") {
-        console.log("ut s");
-        var req = Object.keys(request)[0];
-        var args = request[req];
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if(xhttp.readyState == 4 && xhttp.status == 200) {
-                var res = JSON.parse(xhttp.response);
-                var created_on = new Date(res.created_at).toLocaleDateString().replace(/\/+/g, "-");
-                var updated_on = new Date(res.updated_at).toLocaleDateString().replace(/\/+/g, "-");
-                
-                var html = '<div style="float: right;margin-top: -31px;">' +  
-                                '<span class="ext-meta-container">' +
+(function () {
+    "use strict";
+    /* Multiple requests from same page will be served from this variable instead of pinging Github's API */
+    var isMetaReady = false;
+    var STORAGE = chrome.storage.sync;
+    
+    var bakeHtml = function(metaCreationTimestamp, metaUpdatedTimestamp) {
+        
+        var created_on = new Date(metaCreationTimestamp),
+            updated_on = new Date(metaUpdatedTimestamp),
+            responseHtml = '<div style="float: right;margin-top: -31px;">' +
+                                '<span class="ext-meta-container" title="' + created_on.toLocaleString() + '">' +
                                     '<span class="ext-meta-label">Created</span>' +
-                                    '<span class="ext-meta-value">'+ created_on +'</span>' +
+                                    '<span class="ext-meta-value">' + created_on.toLocaleDateString().replace(/\/+/g, "/") +'</span>' +
                                 '</span>' + 
-                                '<span class="ext-meta-container">' +
+                                '<span class="ext-meta-container" title="' + updated_on.toLocaleString() + '">' +
                                     '<span class="ext-meta-label">Last updated</span>' +
-                                    '<span class="ext-meta-value">'+ updated_on +'</span>' +
+                                    '<span class="ext-meta-value">'+ updated_on.toLocaleDateString().replace(/\/+/g, "/") +'</span>' +
                                 '</span>' +
                             '</div>';
-                
-                sendResponse(html);
+        
+        
+            return responseHtml;
+    };
+    
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        STORAGE.get("metaCache", function(res) {
+
+            var repoURL = request["getRepoMeta"],
+                cache = res.metaCache;
+            
+            
+            if(typeof cache === "object" && cache.hasOwnProperty(repoURL)) {
+                cache = cache[repoURL];
+                if(cache.meta_updated == new Date().toDateString()) {
+                    console.log("sent from cache");
+                    sendResponse(bakeHtml(cache.repo_creation_timestamp, cache.repo_updated_timestamp));
+                    return;
+                }
             }
-        }
-        xhttp.open("GET", "https://api.github.com/repos/" + args, true);
-        xhttp.send();
-    }
-    return true;
-});
+            
+            console.log("Fetching from API");
+            // else get it from Github API
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState === 4 && xhttp.status === 200) {
+                    var res = JSON.parse(xhttp.response),
+                        meta = {};
+
+                    meta[repoURL] = {
+                        "meta_updated" : new Date().toDateString(),
+                        "repo_creation_timestamp" : res.created_at,
+                        "repo_updated_timestamp" : res.pushed_at
+                    };
+
+                    STORAGE.set({"metaCache" : meta});
+                    sendResponse(bakeHtml(res.created_at, res.pushed_at));
+                }
+            };
+
+            xhttp.open("GET", "https://api.github.com/repos/" + repoURL, true);
+            xhttp.send();
+        });
+        return true;
+    });
+}());
--- src/js/injected.js
+++ src/js/injected.js
@@ -1,10 +1,44 @@
-window.onload = function() {
-    var url = window.location.pathname.split("/");
-    var user = url[1];
-    var repo = url[2];
-    var data = user + "/" + repo;
-    chrome.runtime.sendMessage({"getCreationDate" : data}, function(res) {
+(function () {
+    "use strict";
+    var _this = this;
+    _this.repoMetaHtml = "";
+    _this.isMetaLoaded = false;
+    /* Fetch meta for the page ASAP and get it ready for it to be available when window loads.
+     * Callback is optional, it'll passed when window loads, before meta is ready.
+     */
+    _this.getRepoMetaHtml = function (callback) {
+        var url = window.location.pathname.split("/"),
+            user = url[1],
+            repo = url[2],
+            data = user + "/" + repo;
+        chrome.runtime.sendMessage({"getRepoMeta" : data}, function (htmlResponse) {
+            _this.repoMetaHtml = htmlResponse;
+            _this.isMetaLoaded = true;
+            if (callback) {
+                callback();
+            }
+        });
+    };
+    
+    /* Inject the html into page pseudo*/
+    this.insertMetaInPage = function () {
         var targetEl = document.getElementsByClassName("pagehead")[0].getElementsByClassName("container")[1];
-        targetEl.insertAdjacentHTML('beforeend', res);
-    });
-}
+        targetEl.insertAdjacentHTML('beforeend', this.repoMetaHtml);
+    };
+    
+    /* Inject the html into page safely*/
+    this.inject = function () {
+        /* If meta is still not ready, try fetching it again, this time pass the callback to care of the rest */
+        if (!this.isMetaLoaded) {
+            this.getRepoMetaHtml(this.insertMetaInPage);
+        } else {
+            this.insertMetaInPage();
+        }
+    };
+    
+    window.onload = function () {
+        _this.inject();
+    };
+    
+    this.getRepoMetaHtml();
+}).call(this);
